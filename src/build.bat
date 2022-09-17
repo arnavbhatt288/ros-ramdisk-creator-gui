52eddaedbc9b6fe37a17c7d88e70a353c9e096cd
Adam Słaboń <asaillen@protonmail.com>
2022-09-17 17:51:21 +0200

    Add Win32 entrypoint and set SUBSYSTEM to WINDOWS 5.1 (5.2 on x64) on MSVC build

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,3 @@
 @echo off
-cl /nologo /W3 /O2 /fp:fast /Gm- /Feramdisk-creator.exe main.c gdi.c gui.c utils_win.c tinyfiledialogs.c common.c user32.lib gdi32.lib Advapi32.lib Msimg32.lib Comctl32.lib ComDlg32.lib Ole32.lib Shell32.lib /link /incremental:no
+if %VSCMD_ARG_TGT_ARCH%==x64 (set /a ver_minor = 2) else (set /a ver_minor = 1)
+cl /nologo /W3 /O2 /fp:fast /Gm- /Feramdisk-creator.exe main.c gdi.c gui.c utils_win.c tinyfiledialogs.c common.c user32.lib gdi32.lib Advapi32.lib Msimg32.lib Comctl32.lib ComDlg32.lib Ole32.lib Shell32.lib /link /incremental:no /subsystem:windows,5.%ver_minor%
--- src/main.c
+++ src/main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 
 #ifdef _WIN32
+    #include <windows.h>
     #include "gdi.h"
 #elif __linux__
     #include "x11.h"
 #endif
 
+#ifdef _WIN32
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    return start_program();
+}
+#else
 int main()
 {
     return start_program();
 }
+#endif
